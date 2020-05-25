82af6c034a9456d573b4e549710be3583908c68d
WnndGws <WnndGws@users.noreply.github.com>
2020-05-25 20:41:18 +0800

    Minor tweaks after 1st attempt to run

---
+++ 01_base.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# Installs arch linux on the selected drive with LUkS
+# Setup relies on: systemd-boot, UEFI, LVM and luks, netctl, Xorg, bspwm
+
+# Get user input
+read -p "What is the drive you want arch installed on (in the form /dev/sda)?: " drive
+
+echo "Turning off that fucking beep...."
+rmmod pcspkr
+
+# Setup network connection
+#wifi-menu
+echo "Setting up network...."
+sleep 1
+timedatectl set-ntp true
+timedatectl set-timezone Australia/Perth
+
+# Prepare encrypted container
+echo "Preparing luks...."
+modprobe dm-crypt
+cryptsetup luksFormat --type luks2 --cipher aes-xts-plain64 --key-size 512 $drive\2
+cryptsetup open $drive\2 cryptlvm
+
+# Create physical volume
+pvcreate /dev/mapper/cryptlvm
+
+# Create volume group
+vgcreate vg0 /dev/mapper/cryptlvm
+
+# Create logical volumes
+lvcreate -L24G vg0 -n swap
+lvcreate -l 100%FREE vg0 -n root
+
+# Create /
+echo "Mounting drives...."
+mkfs.ext4 /dev/mapper/vg0-root
+mount /dev/mapper/vg0-root /mnt
+
+# Create /boot
+mkfs.fat -F32 $drive\1
+mkdir /mnt/boot
+mount $drive\1 /mnt/boot
+
+# Create swap
+mkswap /dev/mapper/vg0-swap
+swapon /dev/mapper/vg0-swap
+
+# Pacstrap
+echo "Initialising pacstrap...."
+pacstrap /mnt base base-devel linux linux-firmware netctl git zsh vim dialog wpa_supplicant dhcpcd openssh lvm2
+
+# Fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "System will now chroot onto /mnt
+Please curl <++>02 and run that next
+"
+
+# arch-chroot
+arch-chroot /mnt
---
+++ 02_chrooted.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+# Installs arch linux on the selected drive with LUkS
+# Setup relies on: systemd-boot, UEFI, LVM and luks, netctl, Xorg, bspwm
+
+# Get user input
+read -p "What is the hostname?: " hostname
+read -p "What is the username?: " username
+
+# Initial locales
+echo "Setting up locale...."
+ln -sf /usr/share/zoneinfo/Australia/Perth /etc/localtime
+hwclock --systohc
+
+# Locale
+echo "en_AU.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_AU.UTF-8
+LC_COLLATE=C" > /etc/locale.conf
+
+echo "$hostname" > /etc/hostname
+
+echo "127.0.0.1 localhost
+::1 localhost
+127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
+
+# Edit mkinitcpio to move 'keyboard to bfore filesystems, and include encryption
+echo "Setting up mkinitcpio...."
+sed -i 's/\ keyboard//g' /etc/mkinitcpio.conf
+sed -i 's/filesystems/keyboard\ encrypt\ lvm2\ resume\ filesystems/g' /etc/mkinitcpio.conf
+mkinitcpio -p linux
+
+# Systemd boot
+echo "Setting up Systemd-boot...."
+bootctl --path=/boot install
+
+echo "title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options cryptdevice=UUID=$(blkid -s UUID -o value $drive\2):cryptlvm root=/dev/mapper/vg0-root quiet rw" > /boot/loader/entries/arch.conf
+
+echo "default       arch
+timeout       5
+console-mode  max" > /boot/loader/loader.conf
+
+bootctl --path=/boot update
+
+# Add user
+groupadd mediamgmt
+useradd --create-home -G wheel -G audio -G video -G mediamgmt --shell /bin/zsh $username
+passwd $username
+
+echo "uncomment this line in /etc/sudoers:
+%wheel ALL=(ALL) ALL
+"
+echo "Please reboot, and run 03_userspace.sh
+<++>03_userspace"
