3621d72ef4ae787b16f4a8523eafe747e6f0a9a0
WnndGws <WnndGws@users.noreply.github.com>
2020-05-27 09:53:11 +0000

    Create 03_userspace.sh
    Initial commit

---
+++ 03_userspace.sh
@@ -0,0 +1,18 @@
+#!/bin/env zsh
+## Using zsh since that us my user shell, but also so we can use bash-isms
+
+# Remove 02.sh used for install
+echo "Removing 02.sh from root directory...."
+sudo rm /02.sh
+
+# Step 1
+# Wifi
+
+# Step 2
+# Video drivers + xorg
+
+# Step 3
+# Install all utilities
+
+# Step 4
+# Pull and stow dotfiles
