dc61a2c00e9aaf958264f9840012d4ce1ffeecac
WnndGws <WnndGws@users.noreply.github.com>
2020-05-30 02:02:53 +0800

    Add STOWing of dotfiles

---
+++ 04_dotfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/env zsh
+## Using zsh since that us my user shell, but also so we can use bash-isms
+
+printf 'GPG and SSH should not be set up. If it isnt, quit this and set them up or wont be able to clone the git repos'
+
+while true; do
+    read -p "Are the GPG and SSH keys added to github" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+mkdir ~/git && cd ~/git
+git clone git@github.com:wnndgws/dotfiles.git
+git clone git@github.com:wnndgws/scripts.git
+
+cd ~/git/dotfiles
+for i in */; do
+    stow --restow --target=$HOME $i
+done
+zsh
+
+echo "Installing vim plugins. Quit vim to continue"
+vim -c "PlugInstall"
+pikaur -S cmake
+cd $XDG_CONFIG_HOME/vim/plugged/YouCompleteMe
+python3 install.py --all
+
+
