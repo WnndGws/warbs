71f46dbb84e4e3ad4de0bf5e9e12c4e10ac26632
WnndGws <WnndGws@users.noreply.github.com>
2020-12-11 04:01:11 +0800

    Add multilib to pacman.conf

--- 03_userspace.sh
+++ 03_userspace.sh
@@ -87,6 +87,13 @@ sudo systemctl restart iwd.service
 
 # Step 2
 # Video drivers + xorg
+
+# Required for 32bit vulkan support
+echo "Adding multilib to pacman.conf for 32 bit support"
+LINESTART=$(grep -Fnr "[multilib]" /etc/pacman.conf | cut -d : -f1)
+LINEEND=$((LINESTART+1))
+sed -i "${LINESTART},${LINEEND} s/^#//" /etc/pacman.conf
+
 echo "Updating system before continuing...."
 sudo pacman -Syyu
 clear
